b7654210cfd530a05766bc8e1a3144a6af1d7147
John Clements <clements@racket-lang.org>
2022-09-01 20:20:28 -0400

    strings better than regexps

--- info.rkt
+++ info.rkt
@@ -4,6 +4,6 @@
 (define deps '("base"))
 (define copy-foreign-libs '("libportaudio.2.dylib"
                             "librsoundcallbacks.dylib"))
-(define install-platform #rx"aarch64-macosx")
+(define install-platform "aarch64-macosx")
 
 
